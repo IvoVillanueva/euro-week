c51cda19a4da23ee6953b6a6d2d0ea07b888f48d
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2026-01-09 21:41:32 +0100

    Update image source in union_grafico.py

--- scripts/union_grafico.py
+++ scripts/union_grafico.py
@@ -169,8 +169,8 @@ table = theme_savant(
 )
  .tab_source_note(
     md(
-        f"""{caption} <img src="https://substack.com/img/substack.png"
-        style="height:40px;width:40px;vertical-align:middle;"/>"""
+        f"""{caption} <img src='https://supermanager.acb.com/assets/images/logo.svg'
+        style='height:40px;width:40px;vertical-align:middle;'/>"""
     )
 )
 )
